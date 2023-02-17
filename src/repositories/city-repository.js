208cd6e90fd31d5e6d747c579c3143b538ebbaef
Rahul Mahato <mahatorahul025@gmail.com>
2023-02-18 00:23:13 +0530

    completed city repository

--- src/repositories/city-repository.js
+++ src/repositories/city-repository.js
@@ -3,20 +3,44 @@ const { City } = require("../models/index");
 class CityRepository {
   async createCity({ name }) {
     try {
-      await City.create({ name: name });
+      const city = await City.create({ name });
+      return city;
     } catch (error) {
-      throw error;
+      throw { error };
     }
   }
 
-  async deleteCity(id) {
+  async deleteCity(cityId) {
     try {
       await City.destroy({
         where: {
-          id: id,
+          id: cityId,
         },
       });
-    } catch (error) {}
+      return true;
+    } catch (error) {
+      throw { error };
+    }
+  }
+
+  async updateCity(cityId, data) {
+    try {
+      const city = await City.findByPk(cityId);
+      city.name = data.name;
+      await city.save();
+      return city;
+    } catch (error) {
+      throw { error };
+    }
+  }
+
+  async getCity(cityId) {
+    try {
+      const city = await City.findByPk(cityId);
+      return city;
+    } catch (error) {
+      throw { error };
+    }
   }
 }
 
